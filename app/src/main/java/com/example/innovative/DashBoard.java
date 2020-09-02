3b57d3843393371f1cf7bf6401cdcd7088284ef1
sadanand madame <sadanandm1234@gmail.com>
2020-09-02 11:59:32 +0530

    update

--- app/src/main/java/com/example/innovative/DashBoard.java
+++ app/src/main/java/com/example/innovative/DashBoard.java
@@ -8,16 +8,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class DashBoard extends AppCompatActivity {
-    Button btn_math;
+    Button btn_math,btn_sci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
         btn_math = findViewById(R.id.btn_term1);
+        btn_sci = findViewById(R.id.btn_sci);
         btn_math.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent i = new Intent(DashBoard.this,Term.class);
+                startActivity(i);
+
+            }
+        });
+        btn_sci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
                 Intent i = new Intent(DashBoard.this,Term.class);
                 startActivity(i);
             }
